af877a872c28703452bdacbd32ff8dfdc9b9648a
aninhaab00 <aninhaab00@gmail.com>
2024-08-23 11:58:58 -0300

    Add files via upload

---
+++ css/overrideAutodetect.lua
@@ -0,0 +1,16 @@
+FullscreenEnabled = 0	     	   		  	     	       	       
+ResolutionWidth = 1920       	    	  	       	       	       	    
+ResolutionHeight = 1080   	  	  		       	   	   
+OverallGraphicsQuality = 0       	  	     		  	 
+VSyncEnabled = 0	      	     	    	     	  	      	 
+PresentInterval = 0    	  	   	  	   	    	    	 
+WindowedBorderless = 1       	  	  	   	   	      	  
+StrandBasedHair =  -1  	 		 	     	  	   	 
+UseGoalNets3d =  -1 	   	 	 	     	  	      	   
+DynamicResolution =  0   	     	      		    	 	   
+   		 	      	   		      		 	 
+ 	    	       	   	  	     	       	 	 	       
+	  	    		   	  	   	      	       	    
+    	      			     	    		   	      	     
+       	 	      
+
